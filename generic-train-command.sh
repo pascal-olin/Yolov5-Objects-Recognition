25ebc113ac92cb2f66b198377f6306f186b3c9b2
pascal_olin <polin1962@gmail.com>
2022-01-02 07:32:54 +0100

    added generic-train-command

---
+++ generic-train-command.sh
@@ -0,0 +1,2 @@
+(CVision) home@home-pc:~/dev/object-recognition-yolov5/Yolov5-Objects-Recognition/yolov5-6.0-detect$ python train.py --batch 4 --epochs 50 --data ../../../openimages/tempo7/tempo7bis.yaml --project ../../../openimages/tempo7/po4 --weights yolov5s.pt
+
